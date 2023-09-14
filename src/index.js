9dcfd4669d65a554e8542e375646e545d9ff953c
meron <merommichaelahw@gmail.com>
2023-09-14 11:18:49 +0100

    props intro class

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-//1. Create a new React app.
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from "./components/App"
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-//2. Create a App.jsx component.
-//3. Create a Header.jsx component that renders a <header> element
-//to show the Keeper App name in an <h1>.
-//4. Create a Footer.jsx component that renders a <footer> element
-//to show a copyright message in a <p> with a dynamically updated year.
-//5. Create a Note.jsx component to show a <div> element with a
-//<h1> for a title and a <p> for the content.
-//6. Make sure that the final website is styled like the example shown here:
-//https://l1pp6.csb.app/
+function Card(props) {
+    return (
+        <div>
+            <h1>My Contacts</h1>
 
-//HINT: You will need to study the classes in teh styles.css file to appy styling.
+            <h2>{props.name}</h2>
+            <img
+                src={props.img}
+                alt="avatar_img"
+            />
+            <p>{props.tel}</p>
+            <p>{props.email}</p>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <div>
+        <Card name="Beyonce" img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg" tel="+123 456 789" email="b@beyonce.com" />
+        <Card name="Jack Bauer" img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg" tel="+987 654 321" email="jack@nowhere.com" />
+        <Card name="Chuck Norris" img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png" tel="+918 372 574" email="gmail@chucknorris.com" />
+    </div>,
+    document.getElementById("root")
+);
