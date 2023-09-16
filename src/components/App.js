97bb7e83c836438c357e887a15443117fd42c082
meron <merommichaelahw@gmail.com>
2023-09-16 17:13:56 +0100

    spread operator challenge completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,42 @@
 import React, { useState } from "react";
 
 function App() {
-    const [contact, setContact] = useState({
-        fName: "",
-        lName: "",
-        email: ""
-    });
+
+    const [todo, setTodo] = useState("");
+    const [todos, setTodos] = useState([]);  // State to hold the array of todos
 
     function handleChange(event) {
-        const { name, value } = event.target;
+        const newTodo = event.target.value;  // Get the new todo from the input
+        setTodo(newTodo);  // Update the todo state with the new value
+    }
 
-        setContact(prevValue => {
-            return {
-                ...prevValue,
-                [name]: value
-            }
-        });
+    function handleClick() {
+        setTodos(prevTodos => [...prevTodos, todo]);  // Add todo to the todos array
+        setTodo("");  // Clear the input after adding todo to the array
     }
 
     return (
         <div className="container">
-            <h1>
-                Hello {contact.fName} {contact.lName}
-            </h1>
-            <p>{contact.email}</p>
-            <form>
-                <input name="fName" placeholder="First Name" value={contact.fName} onChange={handleChange} />
-                <input name="lName" placeholder="Last Name" value={contact.lName} onChange={handleChange} />
-                <input name="email" placeholder="Email" value={contact.email} onChange={handleChange} />
-                <button>Submit</button>
-            </form>
+            <div className="heading">
+                <h1>To-Do List</h1>
+            </div>
+            <div className="form">
+                <input
+                    type="text"
+                    onChange={handleChange}
+                    value={todo}
+                />
+                <button onClick={handleClick}>
+                    <span>Add</span>
+                </button>
+            </div>
+            <div>
+                <ul>
+                    {todos.map((item, index) => (
+                        <li key={index}>{item}</li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -4,18 +4,8 @@ import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-// const citrus = ["Lime", "Lemon", "Orange"];
-// const fruits = ["Apple", ...citrus, "Banana", "Coconut"];
-
-// const fullName = {
-//   fName: "James",
-//   lName: "Bond"
-// };
-
-// const user = {
-//   ...fullName,
-//   id: 1,
-//   username: "jamesbond007"
-// };
-
-// console.log(user);
+//CHALLENGE: Make this app work by applying what you've learnt.
+//1. When new text is written into the input, its state should be saved.
+//2. When the add button is pressed, the current data in the input should be
+//added to an array.
+//3. The <ul> should display all the array items as <li>s
