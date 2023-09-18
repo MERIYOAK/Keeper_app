cfcaf1d72665b4ad5462ee7353b0b1611aa700f3
meron <merommichaelahw@gmail.com>
2023-09-18 18:34:31 +0100

    adding dependencies like material ui

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
 
 function CreateArea(props) {
     const [note, setNote] = useState({
         title: "",
         content: ""
     });
+    const [isExpanded, setIsExpanded] = useState(false);
 
     function handleChange(event) {
         const { name, value } = event.target;
@@ -26,23 +30,35 @@ function CreateArea(props) {
         event.preventDefault();
     }
 
+    function expand() {
+        setIsExpanded(true);
+    }
+
     return (
         <div>
-            <form>
-                <input
-                    name="title"
-                    onChange={handleChange}
-                    value={note.title}
-                    placeholder="Title"
-                />
+            <form className="create-note">
+                {
+                    isExpanded && <input
+                        name="title"
+                        onChange={handleChange}
+                        value={note.title}
+                        placeholder="Title"
+                    />
+                }
+
                 <textarea
                     name="content"
                     onChange={handleChange}
                     value={note.content}
                     placeholder="Take a note..."
-                    rows="3"
+                    rows={isExpanded ? 3 : 1}
+                    onClick={expand}
                 />
-                <button onClick={submitNote}>Add</button>
+                <Zoom in={isExpanded}>
+                    <Fab onClick={submitNote}>
+                        <AddIcon />
+                    </Fab>
+                </Zoom>
             </form>
         </div>
     );
