77c20d88be4f9ee7233960476c3a27daf9865866
meron <merommichaelahw@gmail.com>
2023-09-18 12:04:45 +0100

    managing component tree practice

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,17 @@
 import React, { useState } from "react";
-import TodoItem from "./TodoItem";  // Import the new component
+import ToDoItem from "./TodoItem";
+import InputArea from "./InputArea";
 
 function App() {
-    const [inputText, setInputText] = useState("");
+
     const [items, setItems] = useState([]);
 
-    function handleChange(event) {
-        const newValue = event.target.value;
-        setInputText(newValue);
-    }
-
-    function addItem() {
+    function addItem(inputText) {
         setItems(prevItems => {
             return [...prevItems, inputText];
         });
-        setInputText("");
     }
+
 
     function deleteItem(id) {
         setItems(prevItems => {
@@ -30,16 +26,16 @@ function App() {
             <div className="heading">
                 <h1>To-Do List</h1>
             </div>
-            <div className="form">
-                <input onChange={handleChange} type="text" value={inputText} />
-                <button onClick={addItem}>
-                    <span>Add</span>
-                </button>
-            </div>
+            <InputArea onAdd={addItem} />
             <div>
                 <ul>
                     {items.map((todoItem, index) => (
-                        <TodoItem key={index} id={index} text={todoItem} onCheck={deleteItem} />
+                        <ToDoItem
+                            key={index}
+                            id={index}
+                            text={todoItem}
+                            onChecked={deleteItem}
+                        />
                     ))}
                 </ul>
             </div>
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,16 @@
-// TodoItem.js
 import React from "react";
 
-function TodoItem(props) {
-
+function ToDoItem(props) {
     return (
-        <li onClick={() => {
-            props.onCheck(props.id)
-        }} >
-            {props.text}
-        </li>
+        <div
+            onClick={() => {
+                props.onChecked(props.id);
+            }}
+        >
+            <li>{props.text}</li>
+        </div>
     );
 }
 
-export default TodoItem;
+export default ToDoItem;
+
